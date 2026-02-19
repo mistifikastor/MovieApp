f00dbbb63c29c69d168676137df9667bb728509c
mistifikastor <menoftoday@yandex.ru>
2026-02-19 17:57:49 +0700

    TO MVC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,9 +74,6 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-
     // Retrofit для API
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
